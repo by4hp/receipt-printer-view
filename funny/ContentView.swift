0a32a8d41fa621db16a7e211f8f71b0e427d40d9
by4hp <by4hpvv@gmail.com>
2025-05-15 14:44:30 +0800

    Initial Commit

---
+++ funny/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  funny
+//
+//  Created by 湖泊 on 2025/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ funny/funnyApp.swift
@@ -0,0 +1,17 @@
+//
+//  funnyApp.swift
+//  funny
+//
+//  Created by 湖泊 on 2025/5/15.
+//
+
+import SwiftUI
+
+@main
+struct funnyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
